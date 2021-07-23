685b910d9b414b289fd1e3b9a6b57aa62559054b
WangXiaojie01 <1259488569@qq.com>
2021-07-23 11:44:15 +0800

    提交代码

---
+++ Assets/ExampleClass.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    public string stringToEdit = "Hello World";
+    private TouchScreenKeyboard keyboard;
+    private string rectStr = "(0, 0, 0, 0)";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TouchScreenKeyboard.visible)
+        {
+            Rect rect = TouchScreenKeyboard.area;
+            rectStr = "(" + rect.x + ", " + rect.y + ", " + rect.width + ", " + rect.height + ")";
+        }
+        else
+        {
+            rectStr = "(0, 0, 0, 0)";
+        }
+    }
+
+    void OnGUI()
+    {
+        GUIStyle txtStyle = new GUIStyle();
+        txtStyle.fontSize = 25;
+        stringToEdit = GUI.TextField(new Rect(10, 10, 300,50), stringToEdit, txtStyle);
+
+        rectStr = GUI.TextField(new Rect(10, 60, 300, 50), rectStr, txtStyle);
+
+        if (GUI.Button(new Rect(10, 110, 200, 100), "Default"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        }
+        if (GUI.Button(new Rect(10, 210, 200, 100), "ASCIICapable"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.ASCIICapable);
+        }
+        if (GUI.Button(new Rect(10, 310, 200, 100), "Numbers and Punctuation"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.NumbersAndPunctuation);
+        }
+        if (GUI.Button(new Rect(10, 410, 200, 100), "URL"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.URL);
+        }
+        if (GUI.Button(new Rect(10, 510, 200, 100), "NumberPad"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.NumberPad);
+        }
+        if (GUI.Button(new Rect(10, 610, 200, 100), "PhonePad"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.PhonePad);
+        }
+        if (GUI.Button(new Rect(10, 710, 200, 100), "NamePhonePad"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.NamePhonePad);
+        }
+        if (GUI.Button(new Rect(10, 810, 200, 100), "EmailAddress"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.EmailAddress);
+        }
+        if (GUI.Button(new Rect(10, 910, 200, 100), "Social"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Social);
+        }
+        if (GUI.Button(new Rect(10, 1010, 200, 100), "Search"))
+        {
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Search);
+        }
+    }
+}
